20974b1195e47aef53b5d35fb9529dbb6984fadf
Rakesh.Chintha <Rakesh.Chintha@neudesic.com>
2016-09-15 16:23:40 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+(function(){
+
+	/*$( "#search" ).focus(function() {
+	  	$( ".search-wrapper" ).addClass( "focused" );
+	}).focusout(function() {
+		$( ".search-wrapper" ).removeClass( "focused" );
+	});*/
+
+	/*$('.cards-container').isotope({
+	  // set itemSelector so .grid-sizer is not used in layout
+	  itemSelector: '.mynote-card',
+	  masonry: {
+	    // use element for option
+	    columnWidth: '.card-sizer'
+	  }
+	})*/
+	
+
+
+	/*$('.settings-modal-trigger').leanModal();*/
+}());
